e64540cb5580d7b23616a4644ec2b20f4e2c0139
progrmmer <84572292+programmer20000@users.noreply.github.com>
2022-04-04 10:54:20 +0300

    was add file project

---
+++ generator_password.py
@@ -0,0 +1,52 @@
+from random import randint
+from colorama import Fore
+from os import system
+from time import sleep
+from datetime import datetime
+import csv
+from symbols_for_password import symbols_for_password
+
+headers = ("date created ", "password")
+
+with open(file="save_password.csv", mode="w") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(headers)
+
+
+def generator_password():
+    print(f'{Fore.RED}Enter count symbol for password only 1 to 68')
+    question_user = int(input(f'{Fore.GREEN}Enter count symbol for password: '))
+
+    date_and_time = datetime.today()
+
+    def message_for_user():
+        print('error')
+        sleep(6)
+        system('cls')
+        exit()
+
+    password = ''
+
+    if question_user > len(symbols_for_password):
+        message_for_user()
+
+    for i in range(question_user):
+        password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
+    with open(file="save_password.csv", mode="a") as file:
+        writer = csv.writer(file)
+        writer.writerow([str(date_and_time),password])
+
+
+while True:
+    generator_password()
+    print("if your want to exit enter (y) if not enter (n)")
+    exit_from_program = input("your want to exit: ")
+
+    if exit_from_program == "y":
+        system('cls')
+        break
+
+    if exit_from_program == "n":
+        system("cls")
+        continue
