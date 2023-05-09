9912f1233019a37ad5050a93bb66b5bbccf67e79
Mihai Apostol <mihaiapostol153@gmail.com>
2023-05-09 12:13:37 +0300

    Replace file format for writing password from csv to excel

--- generator_password.py
+++ generator_password.py
@@ -1,18 +1,37 @@
-from random import randint
-from colorama import Fore
-from os import system
-from time import sleep
 from datetime import datetime
-import csv
+from os import system
+from random import randint
+from time import sleep
+
+import openpyxl
+from colorama import Fore
+
 from symbols_for_password import symbols_for_password
 
+datetime_password = {}
+
+
+def create_excel_file(dictionary: dict):
+    book = openpyxl.Workbook()
+    sheet = book.active
+
+    sheet['A1'] = 'DATA AND TIME'
+    sheet['B1'] = 'PASSWORD'
+    row = 2
+
+    for key, value in dictionary.items():
+        sheet[row][0].value = key
+        sheet[row][1].value = value
+        row += 1
+
+    book.save('yours_password.xlsx')
 
 
 def generator_password():
     print(f'{Fore.RED}Enter count symbol for password only 1 to 2048')
     question_user = int(input(f'{Fore.GREEN}Enter count symbol for password: '))
 
-    date_and_time = datetime.today()
+    date_and_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
     def message_for_user():
         print(f'{Fore.RED} Please enter count symbol for password only 1 to 2048')
@@ -25,25 +44,11 @@ def generator_password():
     if question_user > len(symbols_for_password):
         message_for_user()
 
-
     for i in range(question_user):
         password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
-    date_created_password = f"{date_and_time}  {password}"
+        datetime_password.update({date_and_time: password})
+    create_excel_file(dictionary=datetime_password)
 
-    with open(file="save_password.csv", mode="w") as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            (
-                "password".upper(),
-                "date_created_password".upper(),
-            )
-        )
-
-    with open(file="save_password.csv", mode="a",newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(
-          [date_created_password,password]
-        )
 
 while True:
     generator_password()
